4e5bd2a76dc09e9bf6061851fb96ad2ce4524a6f
Imtiaz Shawn <Imtiazshawn01@gmail.com>
2023-11-27 12:50:53 +0600

    name

--- src/components/FilterStudents.vue
+++ src/components/FilterStudents.vue
@@ -19,7 +19,7 @@
                     {{ data.roll }}
                 </template>
                 <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by name" />
+                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by Roll" />
                 </template>
             </Column>
             <Column field="student_id" header="Student Id" style="min-width: 12rem">
@@ -27,7 +27,7 @@
                     {{ data.student_id }}
                 </template>
                 <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by name" />
+                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by Student Id" />
                 </template>
             </Column>
             <Column field="name" header="Name" style="min-width: 12rem">
@@ -35,7 +35,7 @@
                     {{ data.name }}
                 </template>
                 <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by name" />
+                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by Name" />
                 </template>
             </Column>
             <Column field="grade" header="Grade" style="min-width: 12rem">
@@ -43,7 +43,7 @@
                     {{ data.academic_details.grade }}
                 </template>
                 <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by name" />
+                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by Grade" />
                 </template>
             </Column>
             <Column field="math" header="Math" style="min-width: 12rem">
@@ -51,7 +51,7 @@
                     {{ data.academic_details.marks.math }}
                 </template>
                 <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by name" />
+                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by Marks" />
                 </template>
             </Column>
             <Column field="science" header="Science" style="min-width: 12rem">
@@ -59,7 +59,7 @@
                     {{ data.academic_details.marks.science }}
                 </template>
                 <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by name" />
+                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by Marks" />
                 </template>
             </Column>
             <Column field="english" header="English" style="min-width: 12rem">
@@ -67,7 +67,7 @@
                     {{ data.academic_details.marks.english }}
                 </template>
                 <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by name" />
+                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by Marks" />
                 </template>
             </Column>
             <Column field="contact" header="Contact" style="min-width: 12rem">
@@ -75,7 +75,7 @@
                     {{ data.guardian_details.contact }}
                 </template>
                 <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by name" />
+                    <InputText v-model="filterModel.value" type="text" class="p-column-filter" placeholder="Search by Contact Number" />
                 </template>
             </Column>
         </DataTable>
@@ -83,47 +83,46 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { StudentsList } from '../api/StudentsList';
-import { FilterMatchMode, FilterOperator } from 'primevue/api';
+    import { ref, onMounted } from 'vue';
+    import { StudentsList } from '../api/StudentsList';
+    import { FilterMatchMode, FilterOperator } from 'primevue/api';
 
-const students = ref();
-const filters = ref();
-const loading = ref(true);
+    const students = ref();
+    const filters = ref();
+    const loading = ref(true);
 
-onMounted(() => {
-    StudentsList.getStudentsListMedium().then((data) => {
-        students.value = getStudents(data);
-        loading.value = false;
+    onMounted(() => {
+        StudentsList.getStudentsListMedium().then((data) => {
+            students.value = getStudents(data);
+            loading.value = false;
+        });
     });
-});
 
-
-const initFilters = () => {
-    filters.value = {
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        roll: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-        student_id: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-        name: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-        grade: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-        math: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-        science: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-        english: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-        contact: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+    const initFilters = () => {
+        filters.value = {
+            global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+            roll: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+            student_id: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+            name: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+            grade: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+            math: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+            science: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+            english: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+            contact: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+        };
     };
-};
 
-initFilters();
-
-const clearFilter = () => {
     initFilters();
-};
 
-const getStudents = (data) => {
-    return [...(data || [])].map((d) => {
-        d.date = new Date(d.date);
+    const clearFilter = () => {
+        initFilters();
+    };
 
-        return d;
-    });
-};
+    const getStudents = (data) => {
+        return [...(data || [])].map((d) => {
+            d.date = new Date(d.date);
+
+            return d;
+        });
+    };
 </script>
